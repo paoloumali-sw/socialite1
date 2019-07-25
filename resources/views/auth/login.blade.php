a374a7888d1b01cba1d94f94ec9a6b9ea3da5365
paoloumali-sw <paolo.umali@solarwinds.cloud>
2019-07-25 17:06:27 +0800

    basic login with google controller

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -51,7 +51,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
+                        <div class="form-group row">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
@@ -62,6 +62,14 @@
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
+                            </div>
+                        </div>
+
+                        <hr />
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <a href="/login/google" class="btn btn-info">Login with Google</a>
                             </div>
                         </div>
                     </form>
